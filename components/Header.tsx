29fa0df1c4af484167813ab77e97bcd3e4c8f247
BranGitfox <vixfgit@gmail.com>
2024-10-19 22:24:43 +0300

    header:done

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,21 +11,22 @@ import {FontAwesome} from '@expo/vector-icons'
 export default function Header() {
   return (
     <View style={style.container}>
-      <View>
+      <View style={style.left}>
         <Image
           style={style.logo}
           source={require("../assets/images/mgstore.png")}
         />
-        <Text style={style.title}></Text>
+        <Text style={style.title}>MG-Store</Text>
       </View>
       <View>
-      <FontAwesome name='shopping-cart' size={28} color='white'/>
+            <FontAwesome name='shopping-cart' size={28} color='white'/>
       </View>
     </View>
   );
 }
 
 const style = StyleSheet.create({
+
   logo: {
     width: 90,
     height: 90,
@@ -35,11 +36,22 @@ const style = StyleSheet.create({
     flexDirection: "row",
     height: 90,
     backgroundColor: "green",
-    alignItems:'center'
+    alignItems:'center',
+    justifyContent:'space-between',
+    padding:10
   },
 
   title: {
     color: "white",
     fontSize: 20,
+    fontWeight:'bold'
   },
+
+  left:{
+    flex:2/3,
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-between',
+  }
+  
 });
